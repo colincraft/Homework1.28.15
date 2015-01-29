88c0e7571045ef1ad27fa92ee6f78dc63624302f
Colin Craft <colin@colincraft.com>
2015-01-28 18:02:04 -0800

    getJSON solution

--- javascript.js
+++ javascript.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
 
+// ajax solution
+// function getMovieTitle(movie){
+// 		$.ajax({
+// 			url:"http://www.omdbapi.com/?t=" + movie,
+// 			success: function(data){
+// 				var movie = JSON.parse(data);
+// 				console.log(movie.Title);
+// 				var titleName = $("<p>").text(movie.Title);
+// 				$("body").append(titleName);
+// 			}
+// 		});	
+// 	}
 
+//getJSON solution
 function getMovieTitle(movie){
-		$.ajax({
-			url:"http://www.omdbapi.com/?t=" + movie,
-			success: function(data){
-				var movie = JSON.parse(data);
-				console.log(movie.Title);
-				var titleName = $("<p>").text(movie.Title);
-				$("body").append(titleName);
-			}
-		});	
-	}
+	$.getJSON("http://www.omdbapi.com/?t=" + movie, function(data){
+		var titleName = $("<p>").text(data.Title);
+		$("body").append(titleName);
 
+	});
+}
 	// try getJSON and done
 	$("form").on("submit", function(e){
 		var movie = $("#movie").val();
